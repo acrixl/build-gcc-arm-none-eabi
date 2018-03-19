23cbca7269ac0e5a2774929c10b6491c83ebf665
acr <acrsofter@gmail.com>
2018-03-19 15:35:03 +0700

    R1

--- 20_make_tarball.sh
+++ 20_make_tarball.sh
@@ -3,7 +3,5 @@ CDIR="$(pwd)"
 
 . settings.sh
 
-ln -fs ${PKG_NAME}_$PKG_VERSION $TARGET_TMP_PREFIX/$PKG_NAME
-
-tar --checkpoint=.100 --transform "s,^,$PKG_PREFIX/," -cjf $TAR_DIR/$TAR_NAME.tar.bz2 -C $TARGET_TMP_PREFIX .
+tar --checkpoint=.100 -cjf $TAR_DIR/$TAR_NAME.tar.bz2 -C $TARGET_TMP_PREFIX .
 
